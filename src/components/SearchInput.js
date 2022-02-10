fe1ccdfa5fe31a2a58d04e63d255ebf95f5af1d0
Mahmoud El Helou <mahelhelou@gmail.com>
2022-02-10 09:16:26 +0200

    React movie app is now completed, but needs some improvements to work on later on

---
+++ src/components/SearchInput.js
@@ -0,0 +1,9 @@
+const SearchInput = props => {
+	return (
+		<div className='col col-sm-4'>
+			<input className='form-control' value={props.value} onChange={e => props.setSearchValue(e.target.value)} placeholder='Type to search...' />
+		</div>
+	)
+}
+
+export default SearchInput
